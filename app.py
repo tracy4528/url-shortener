0ddf193cd4e20730503b2c3471e9c5b1eeccbbbe
Tracy <tracy4528@gmail.com>
2024-09-04 11:12:44 +0800

    init flask

---
+++ app.py
@@ -0,0 +1,21 @@
+
+import os
+from datetime import datetime, timedelta
+from flask import Flask, request, jsonify, redirect
+from flask_sqlalchemy import SQLAlchemy
+import string
+import random
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
